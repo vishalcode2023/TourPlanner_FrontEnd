1a9696a37dc83e98c0a3e9133dacdb8bc7f42d79
vishalcode2023 <151914635+vishalcode2023@users.noreply.github.com>
2025-04-06 20:57:46 +0530

    Update Axios.jsx

--- src/Components/Axios/Axios.jsx
+++ src/Components/Axios/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://tourplanner-backend.onrender.com", 
+  baseURL: "https://tourplanner-backend.onrender.com/api", 
   withCredentials: true, 
 });
 
